375f811b622dae5510ce02931e2e7c5690feea6f
Sidhart <siddharthashuklajee8@gmail.com>
2026-04-04 03:46:28 +0530

    fix: final google oauth callback

--- Backend/src/config/passport.js
+++ Backend/src/config/passport.js
@@ -11,7 +11,10 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "/api/auth/google/callback",
+            callbackURL:
+  process.env.NODE_ENV === "production"
+    ? "https://interview-ai-backend-d3j3.onrender.com/api/auth/google/callback"
+    : "http://localhost:3000/api/auth/google/callback"
         },
         async (_accessToken, _refreshToken, profile, done) => {
             try {
